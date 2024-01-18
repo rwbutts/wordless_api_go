24c54d747f08252d8d334f1057bad0ffb06884f1
Ronald W. (Bill) Butts <bill@rwbutts.net>
2024-01-18 16:50:09 -0500

    V1.1.0 - add ./wwwroot static file server

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wordless_api_go
-Backend API for wordless.vue written in GO
+V1.1.0
+Backend API and static file server for wordless.vue written in GO.  
+Serves static content out of ./wwwroot, with default file of "index.html".
+
+./wwwroot is where the deployment of project "wordless_vue" is copied.
+
--- main/Main.go
+++ main/Main.go
@@ -8,11 +8,14 @@ const LISTEN_ADDRESS = ":5090"
 
 func main() {
 
+	fs := http.FileServer(http.Dir("./wwwroot"))
+
 	http.HandleFunc("/api/healthcheck", handleHealthCheck)
 	http.HandleFunc("/api/randomword", handleGetRandomWord)
 	http.HandleFunc("/api/getword/", handleGetWord)
 	http.HandleFunc("/api/checkword/", handleCheckWord)
 	http.HandleFunc("/api/querymatchcount", handleMatchCount)
+	http.Handle("/", fs)
 
 	err := http.ListenAndServe(LISTEN_ADDRESS, nil)
 	if err != nil {
