1c8e587518679997297932874a8dad96aef168e1
Ronald W. (Bill) Butts <bill@rwbutts.net>
2024-01-18 19:08:08 -0500

    v1.2.0 return 404 when directory listing would be previously given

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # wordless_api_go
-V1.1.0
+v1.1.0
 Backend API and static file server for wordless.vue written in GO.  
 Serves static content out of ./wwwroot, with default file of "index.html".
 
 ./wwwroot is where the deployment of project "wordless_vue" is copied.
 
+v1.2.0
+Augment http.FileServer functionality to return 404 error when a directory listing would normally be returned by FileServer. Just for extra security.
--- main/Main.go
+++ main/Main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
+	"path/filepath"
 )
 
 const LISTEN_ADDRESS = ":5090"
 
 func main() {
 
-	fs := http.FileServer(http.Dir("./wwwroot"))
+	fs := http.FileServer(noListFileSystem{http.Dir("./wwwroot")})
 
 	http.HandleFunc("/api/healthcheck", handleHealthCheck)
 	http.HandleFunc("/api/randomword", handleGetRandomWord)
@@ -22,4 +23,49 @@ func main() {
 		panic(err.Error())
 	}
 
+}
+
+type noListFileSystem struct {
+	fs http.FileSystem
+}
+
+/*
+Open() functon for noListFileSystem struct that wraps the normal fs.Open functionality.
+Inhibits the defauk FileServer "if it'a a direectory without index.htm,
+show a directory listing "functionality.  With this, it will return a file not found
+error, which results instead in a 404 not found rather than serving dir listing.
+*/
+func (nfs noListFileSystem) Open(path string) (http.File, error) {
+	/*
+		If neither file nor directory, just return error so http returns 404.
+	*/
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		It exists, see if its a directory. That's the case we're interested in
+		for special handling.
+	*/
+	s, _ := f.Stat()
+
+	/*
+		If entry is a directory, check if index,html exists. If not, let
+		http return a file not found error; if it does, just let http do what
+		it normally does, return the index.html in the dir.
+	*/
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
